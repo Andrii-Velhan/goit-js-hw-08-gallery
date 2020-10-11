2468f7817086d70a29cef7735c33b86711d88f4a
Andrii-Velhan <velganaa@gmail.com>
2020-10-11 22:18:18 +0300

    without working buttons: left, right
    without working buttons: left, right

--- src/task.js
+++ src/task.js
@@ -1,14 +1,20 @@
 import imageRef from "./gallery-items.js";
 
-console.log(imageRef);
+//console.log(imageRef);
 
 const galleryContainer = document.querySelector('.js-gallery');
 const cardsMarkup = createImageCardsMarkup(imageRef);
-
-
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
-
 galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+const modalOpenImage = document.querySelector('.lightbox__image');
+const containerModalOpen = document.querySelector('div.lightbox');
+const buttonModalClick = document.querySelector('button[data-action="close-lightbox"]');
+const containerModalClose = document.querySelector('.lightbox__overlay');
+
+buttonModalClick.addEventListener('click', onCloseModal);
+containerModalClose.addEventListener('click', onBackdropClick)
+
 
 function createImageCardsMarkup(imageRef) {
   return imageRef
@@ -33,18 +39,59 @@ function createImageCardsMarkup(imageRef) {
 }
 
 function onGalleryContainerClick(evt) {
-  const isImageSwatchEl = evt.target.classList.contains('js-gallery');
+  evt.preventDefault();
+  const isImageSwatchEl = evt.target.nodeName === 'IMG';
 
   if (!isImageSwatchEl) {
     return;
   }
+  console.log('target', evt.target)
+  console.log('currentTarget', evt.currentTarget)
 
-  const swatchEl = evt.target;
-  const parentImageCard = swatchEl.closest('gallery__item');
+  onOpenModalClick()
+  onAddModalImage(evt)
 
+  // const swatchEl = evt.target;
+  // const parentImageCard = swatchEl.closest('gallery__item');
   //removeActiveCardClass();
   //addActiveCardClass(parentImageCard);
   //setBodyBgColor(swatchEl.dataset.source);
+}
+
+function onOpenModalClick() {
+  containerModalOpen.classList.add('is-open');
+  window.addEventListener('keydown', onEscKeyPress);
+  // document.body.classList.add('show-modal');
+}
+
+function onAddModalImage(evt) {
+  modalOpenImage.src = evt.target.dataset.source;
+  modalOpenImage.alt = evt.target.alt;
+}
+
+function onCloseModal(evt) {
+  //galleryContainer.removeEventListener('click', onGalleryContainerClick);
+  containerModalOpen.classList.remove('is-open');
+  window.removeEventListener('keydown', onEscKeyPress);
+  modalOpenImage.src = '';
+  modalOpenImage.alt = '';
+  document.body.classList.remove('show-modal');
+}
+
+function onBackdropClick(evt) {
+  if (evt.target === containerModalClose) {
+    console.log('Кликнули именно в бекдроп!!!!');
+    onCloseModal();
+  }
+}
+
+function onEscKeyPress(evt) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = evt.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onCloseModal();
+  }
 }
 
 // function setBodyBgColor(source) {
